b45fc6fb0fa177047d558903ed608c6eea29cbbd
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-07-26 14:42:29 +0000

    Adding product folder and detail.html page for single product page.

--- store/views.py
+++ store/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Product
+
 
 def categories(request):
     return {
         'categories': Category.objects.all()
-        }
+    }
+
 
 def all_products(request):
-        products = Product.objects.all()
-        return render(request, 'store/main.html', {'products': products})
+    products = Product.objects.all()
+    return render(request, 'store/main.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
+
+
+    
+
+
