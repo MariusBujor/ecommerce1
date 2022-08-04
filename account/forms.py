0ec31e2df047dd5e04c91d176e7894985ddfa28b
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-08-04 11:28:32 +0000

    Addig validations checks to the regiter  form

--- account/forms.py
+++ account/forms.py
@@ -3,14 +3,35 @@ from .models import UserBase
 
 
 class RegistrationForm(forms.ModelForm):
-        user_name = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
-        email = forms.EmailField(max_length=100, help_text='Required', error_messages={'required': 'Sorry, you will need an email'})
-        password = forms.CharField(label='Password', widget=forms.PasswordInput)
-        password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    user_name = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required', error_messages={'required': 'Sorry, you will need an email'})
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
-        class Meta:
-            model = UserBase
-            fields = ('user_name', 'email',)
+    class Meta:
+        model = UserBase
+        fields = ('user_name', 'email',)
+
+    def clean_username(self):
+        user_name = self.cleaned_data['user_name'].lower()
+        r = UserBase.objects.filter(user_name=user_name)
+        if r.count():
+            raise forms.ValidationError("Username already exists")
+        return user_name
+
+    def clean_password(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match.')
+        return cd['password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if UserBase.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                'Please use another Email, that is already taken')
+        return email
+
             
 
 
