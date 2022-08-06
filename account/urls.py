20075151b3d8e297f5dee0bd9517ca32c884de5f
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-08-06 10:44:24 +0000

    Using auth_views from django for the login in view

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from .forms import (UserLoginForm)
 from . import views
 # from .forms import (PwdResetConfirmForm, PwdResetForm, UserLoginForm)
 
@@ -6,6 +9,8 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+        path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', 
+                                                    form_class=UserLoginForm), name='login'),
         path('register/', views.account_register, name='register'),
         path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
         # User Dashboard
