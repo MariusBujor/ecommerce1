d6b90b25acb58c31c2da54cf3a580bf36ff3718e
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-08-11 17:50:16 +0000

    Add the stripe payment element / card

--- static/payment/index.js
+++ static/payment/index.js
@@ -1,1 +1,3 @@
-var stripe = Stripe('pk_test_51LVDiHFR4QsZUb5pbOrLsqC1EyFjDyKyjdWCioeB9yu5uD862wCWUwdClDTx2gsH4ReQIZvh6XJniz3O11KXqybW00unW5awdd')
+var stripe = Stripe('pk_test_51LVDiHFR4QsZUb5pbOrLsqC1EyFjDyKyjdWCioeB9yu5uD862wCWUwdClDTx2gsH4ReQIZvh6XJniz3O11KXqybW00unW5awdd');
+
+var elem = document.getElementById('submit');
