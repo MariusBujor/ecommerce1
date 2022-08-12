7a80dac58f19af7c5ad2df5e4e15134436e4274f
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-08-12 15:03:17 +0000

    Add an ajax request to send the order to the server and create a new order in database

--- orders/models.py
+++ orders/models.py
@@ -26,7 +26,6 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return str(self.id)
