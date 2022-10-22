ecb19f6e2799e194a488cbabcf0ee04b938a5490
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-10-22 23:57:54 +0000

    Set up custom 404 page to show using wildcard path

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
 from .sitemaps import CategorySitemap, ProductSitemap
 
 
 sitemaps = {
-    'category':CategorySitemap,
-    'product':ProductSitemap
+    'category': CategorySitemap,
+    'product': ProductSitemap
 }
 
 urlpatterns = [
@@ -18,6 +18,9 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('orders/', include('orders.urls')),
     path('', include('store.urls', namespace='store')),
+    re_path(r'^.*$', TemplateView.as_view(template_name="errors/404.html",
+                                          content_type="text/html"),
+            ),
     path('robots.txt',
          TemplateView.as_view(template_name="robots.txt",
                               content_type="text/plain"),
@@ -27,5 +30,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = "core.views.page_not_found_view"
--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.shortcuts import render
-
-
-def page_not_found_view(request, exception):
-    return render(request, 'errors/404.html')
