9350fb375d790dcba049a739281cbc8fe818b9cd
Marius  Bujor <51386087+MariusBujor@users.noreply.github.com>
2023-01-27 21:10:25 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ DEBUG = DEVELOPMENT
 
 ENVIRONMENT = env('ENVIRON', default='DEVELOPMENT')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='*').split(","), 'web-production-d4e6.up.railway.app'
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', 'web-production-d4e6.up.railway.app', default='*').split(",")
 
 # Application definition
 
